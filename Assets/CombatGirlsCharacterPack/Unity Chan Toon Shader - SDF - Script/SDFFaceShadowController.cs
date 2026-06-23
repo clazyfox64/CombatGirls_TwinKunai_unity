1a11c11181b2aa04948e227583ccbdda5c770379
mephiblin <mephi.blin@gmail.com>
2026-06-23 18:08:26 +0900

    01

--- Assets/CombatGirlsCharacterPack/Unity Chan Toon Shader - SDF - Script/SDFFaceShadowController.cs
+++ Assets/CombatGirlsCharacterPack/Unity Chan Toon Shader - SDF - Script/SDFFaceShadowController.cs
@@ -1,173 +1,176 @@
 using UnityEngine;
 
-[ExecuteAlways]
-public class SDFFaceShadowController : MonoBehaviour
+namespace CombatGirlsCharacterPack
 {
-    public enum BoneAxis
+    [ExecuteAlways]
+    public class SDFFaceShadowController : MonoBehaviour
     {
-        Forward,
-        Backward,
-        Right,
-        Left,
-        Up,
-        Down
-    }
-
-    [Header("References")]
-    public Transform headBone;
-    public Renderer[] faceRenderers;
-
-    [Header("Bone Orientation")]
-    public BoneAxis headForwardAxis = BoneAxis.Forward;
-    public BoneAxis headRightAxis = BoneAxis.Right;
-
-    [Header("Shader Properties")]
-    public bool forceSDFShadow = true;
-    public bool enableSDFShadow = true;
-    public bool forceScriptVectors = true;
-    public string useSDFShadowProp = "_UseSDFShadow";
-    public string useScriptVectorsProp = "_UseScriptVectors";
-    public string faceForwardProp = "_FaceForward";
-    public string faceRightProp = "_FaceRight";
-
-    private MaterialPropertyBlock propBlock;
-
-    private void OnEnable()
-    {
-        Apply();
-    }
-
-    private void LateUpdate()
-    {
-        Apply();
-    }
-
-    private void OnValidate()
-    {
-        Apply();
-    }
-
-    private void Apply()
-    {
-        if (headBone == null || !HasAnyRenderer())
+        public enum BoneAxis
         {
-            return;
+            Forward,
+            Backward,
+            Right,
+            Left,
+            Up,
+            Down
         }
 
-        Vector3 forward = SafeNormalize(GetAxisVector(headBone, headForwardAxis), transform.forward);
-        Vector3 right = SafeNormalize(GetAxisVector(headBone, headRightAxis), transform.right);
+        [Header("References")]
+        public Transform headBone;
+        public Renderer[] faceRenderers;
 
-        if (faceRenderers == null)
+        [Header("Bone Orientation")]
+        public BoneAxis headForwardAxis = BoneAxis.Forward;
+        public BoneAxis headRightAxis = BoneAxis.Right;
+
+        [Header("Shader Properties")]
+        public bool forceSDFShadow = true;
+        public bool enableSDFShadow = true;
+        public bool forceScriptVectors = true;
+        public string useSDFShadowProp = "_UseSDFShadow";
+        public string useScriptVectorsProp = "_UseScriptVectors";
+        public string faceForwardProp = "_FaceForward";
+        public string faceRightProp = "_FaceRight";
+
+        private MaterialPropertyBlock propBlock;
+
+        private void OnEnable()
         {
-            return;
+            Apply();
         }
 
-        for (int i = 0; i < faceRenderers.Length; i++)
+        private void LateUpdate()
         {
-            ApplyToRenderer(faceRenderers[i], forward, right);
-        }
-    }
-
-    private bool HasAnyRenderer()
-    {
-        if (faceRenderers == null)
-        {
-            return false;
+            Apply();
         }
 
-        for (int i = 0; i < faceRenderers.Length; i++)
+        private void OnValidate()
         {
-            if (faceRenderers[i] != null)
+            Apply();
+        }
+
+        private void Apply()
+        {
+            if (headBone == null || !HasAnyRenderer())
             {
-                return true;
+                return;
+            }
+
+            Vector3 forward = SafeNormalize(GetAxisVector(headBone, headForwardAxis), transform.forward);
+            Vector3 right = SafeNormalize(GetAxisVector(headBone, headRightAxis), transform.right);
+
+            if (faceRenderers == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < faceRenderers.Length; i++)
+            {
+                ApplyToRenderer(faceRenderers[i], forward, right);
             }
         }
 
-        return false;
-    }
-
-    private void ApplyToRenderer(Renderer targetRenderer, Vector3 forward, Vector3 right)
-    {
-        if (targetRenderer == null)
+        private bool HasAnyRenderer()
         {
-            return;
+            if (faceRenderers == null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < faceRenderers.Length; i++)
+            {
+                if (faceRenderers[i] != null)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
-        if (propBlock == null)
+        private void ApplyToRenderer(Renderer targetRenderer, Vector3 forward, Vector3 right)
         {
-            propBlock = new MaterialPropertyBlock();
-        }
+            if (targetRenderer == null)
+            {
+                return;
+            }
 
-        targetRenderer.GetPropertyBlock(propBlock);
-        WriteSDFProperties(propBlock, targetRenderer, forward, right);
-        targetRenderer.SetPropertyBlock(propBlock);
+            if (propBlock == null)
+            {
+                propBlock = new MaterialPropertyBlock();
+            }
 
-        Material[] sharedMaterials = targetRenderer.sharedMaterials;
-        for (int i = 0; i < sharedMaterials.Length; i++)
-        {
-            targetRenderer.GetPropertyBlock(propBlock, i);
+            targetRenderer.GetPropertyBlock(propBlock);
             WriteSDFProperties(propBlock, targetRenderer, forward, right);
-            targetRenderer.SetPropertyBlock(propBlock, i);
-        }
-    }
+            targetRenderer.SetPropertyBlock(propBlock);
 
-    private void WriteSDFProperties(MaterialPropertyBlock block, Renderer targetRenderer, Vector3 forward, Vector3 right)
-    {
-        if (forceSDFShadow)
-        {
-            block.SetFloat(useSDFShadowProp, enableSDFShadow ? 1.0f : 0.0f);
-        }
-        else
-        {
-            block.SetFloat(useSDFShadowProp, GetMaterialFloat(targetRenderer, useSDFShadowProp, 0.0f));
+            Material[] sharedMaterials = targetRenderer.sharedMaterials;
+            for (int i = 0; i < sharedMaterials.Length; i++)
+            {
+                targetRenderer.GetPropertyBlock(propBlock, i);
+                WriteSDFProperties(propBlock, targetRenderer, forward, right);
+                targetRenderer.SetPropertyBlock(propBlock, i);
+            }
         }
 
-        if (forceScriptVectors)
+        private void WriteSDFProperties(MaterialPropertyBlock block, Renderer targetRenderer, Vector3 forward, Vector3 right)
         {
-            block.SetFloat(useScriptVectorsProp, 1.0f);
+            if (forceSDFShadow)
+            {
+                block.SetFloat(useSDFShadowProp, enableSDFShadow ? 1.0f : 0.0f);
+            }
+            else
+            {
+                block.SetFloat(useSDFShadowProp, GetMaterialFloat(targetRenderer, useSDFShadowProp, 0.0f));
+            }
+
+            if (forceScriptVectors)
+            {
+                block.SetFloat(useScriptVectorsProp, 1.0f);
+            }
+            else
+            {
+                block.SetFloat(useScriptVectorsProp, GetMaterialFloat(targetRenderer, useScriptVectorsProp, 0.0f));
+            }
+            block.SetVector(faceForwardProp, new Vector4(forward.x, forward.y, forward.z, 0.0f));
+            block.SetVector(faceRightProp, new Vector4(right.x, right.y, right.z, 0.0f));
         }
-        else
-        {
-            block.SetFloat(useScriptVectorsProp, GetMaterialFloat(targetRenderer, useScriptVectorsProp, 0.0f));
-        }
-        block.SetVector(faceForwardProp, new Vector4(forward.x, forward.y, forward.z, 0.0f));
-        block.SetVector(faceRightProp, new Vector4(right.x, right.y, right.z, 0.0f));
-    }
 
-    private static float GetMaterialFloat(Renderer targetRenderer, string propertyName, float fallback)
-    {
-        Material sharedMaterial = targetRenderer.sharedMaterial;
-        if (sharedMaterial == null || !sharedMaterial.HasProperty(propertyName))
+        private static float GetMaterialFloat(Renderer targetRenderer, string propertyName, float fallback)
         {
-            return fallback;
+            Material sharedMaterial = targetRenderer.sharedMaterial;
+            if (sharedMaterial == null || !sharedMaterial.HasProperty(propertyName))
+            {
+                return fallback;
+            }
+
+            return sharedMaterial.GetFloat(propertyName);
         }
 
-        return sharedMaterial.GetFloat(propertyName);
-    }
-
-    private static Vector3 SafeNormalize(Vector3 value, Vector3 fallback)
-    {
-        return value.sqrMagnitude > 0.000001f ? value.normalized : fallback.normalized;
-    }
-
-    private static Vector3 GetAxisVector(Transform target, BoneAxis axis)
-    {
-        switch (axis)
+        private static Vector3 SafeNormalize(Vector3 value, Vector3 fallback)
         {
-            case BoneAxis.Forward:
-                return target.forward;
-            case BoneAxis.Backward:
-                return -target.forward;
-            case BoneAxis.Right:
-                return target.right;
-            case BoneAxis.Left:
-                return -target.right;
-            case BoneAxis.Up:
-                return target.up;
-            case BoneAxis.Down:
-                return -target.up;
-            default:
-                return target.forward;
+            return value.sqrMagnitude > 0.000001f ? value.normalized : fallback.normalized;
+        }
+
+        private static Vector3 GetAxisVector(Transform target, BoneAxis axis)
+        {
+            switch (axis)
+            {
+                case BoneAxis.Forward:
+                    return target.forward;
+                case BoneAxis.Backward:
+                    return -target.forward;
+                case BoneAxis.Right:
+                    return target.right;
+                case BoneAxis.Left:
+                    return -target.right;
+                case BoneAxis.Up:
+                    return target.up;
+                case BoneAxis.Down:
+                    return -target.up;
+                default:
+                    return target.forward;
+            }
         }
     }
 }
